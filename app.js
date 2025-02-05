a20b610613425f3b1b63da9c4e43e20863a712ca
RomannaBidnyk <romanna.bidnyk@gmail.com>
2025-02-04 19:22:56 -0500

    Week 12 - jobs

--- app.js
+++ app.js
@@ -3,6 +3,11 @@ require("express-async-errors");
 const express = require("express");
 const app = express();
 
+const helmet = require("helmet");
+const cors = require("cors");
+const xss = require("xss-clean");
+const rateLimiter = require("express-rate-limit");
+
 const connectDB = require("./db/connect");
 
 const authRouter = require("./routes/auth");
@@ -10,12 +15,24 @@ const jobsRouter = require("./routes/jobs");
 
 const authenticatedUser = require("./middleware/authentication");
 
-// error handler
 const notFoundMiddleware = require("./middleware/not-found");
 const errorHandlerMiddleware = require("./middleware/error-handler");
 
+app.set("trust proxy", 1);
+app.use(
+  rateLimiter({
+    windowMs: 15 * 60 * 1000,
+    max: 100,
+  })
+);
 app.use(express.json());
-// extra packages
+app.use(helmet());
+app.use(cors());
+app.use(xss());
+
+app.get("/", (req, res) => {
+  res.send("Jobs API");
+});
 
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/jobs", authenticatedUser, jobsRouter);
